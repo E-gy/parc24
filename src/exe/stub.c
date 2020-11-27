b365cbabfe2b170b998a43c66037aaf981fdf6d9
E-gy <balakhelisey@gmail.com>
2020-11-27 17:11:15 +0100

    Primitive types for all (2)

--- src/exe/stub.c
+++ src/exe/stub.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <parc24/ptypes.h>
+#include <ptypes.h>
 
 int main(int argc, string_mut args[]){
 	printf("!~%s~!\n", argc > 1 ? args[1] : "(v.v)");
