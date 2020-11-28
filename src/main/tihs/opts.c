2805cd2996135187793a672b79848c37bd55a6df
E-gy <balakhelisey@gmail.com>
2020-11-28 17:56:19 +0100

    tihs +-O support

--- src/main/tihs/opts.c
+++ src/main/tihs/opts.c
@@ -6,6 +6,8 @@
 
 #define tihsopts_default ((struct tihsopts){.parcopts = parc_options_default})
 
+#define printopt(opt, reusable) do { if(printshopt == 1) io.logger("shopt %s %s", opts.parcopts. ## opt ? "-s" : "-u", #opt); else if(printshopt == 2) io.logger("%s	%s", #opt, opts.parcopts. ## opt ? "on" : "off"); } while(0)
+
 TihsOptsParseResult tihsopts_parse(argsarr args, ParC24IO io){
 	if(!args) return Error_T(tihsopts_parse_result, {"Args array invalid"});
 	struct tihsopts opts = tihsopts_default;
@@ -13,13 +15,26 @@ TihsOptsParseResult tihsopts_parse(argsarr args, ParC24IO io){
 		//TODO long options stuff
 	}
 	bool readfromstdin = false;
+	int printshopt = 0;
 	for(; *args && (strpref("-", *args) || strpref("+", *args)); args++){
 		if(streq("-c", *args)){
 			if(!*++args) return Error_T(tihsopts_parse_result, {"-c requires an argument"});
 			opts.commandstr = *args++;
 			break;
 		} else if(streq("-s", *args)) readfromstdin = true;
-		//TODO more short options
+		else if(streq("-O", *args) || streq("+O", *args)){
+			bool setto = **args == '+' ? false : true;
+			printshopt = !*++args ? 1+!!setto : 0;
+		}
+	}
+	if(printshopt){
+		printopt(dotglob, setto);
+		printopt(extglob, setto);
+		printopt(nocaseglob, setto);
+		printopt(nullglob, setto);
+		printopt(expand_aliases, setto);
+		printopt(sourcepath, setto);
+		printopt(xpg_echo, setto);
 	}
 	if(!(opts.commandstr || readfromstdin) && *args) opts.commandfile = *args;
 	opts.args = args;
