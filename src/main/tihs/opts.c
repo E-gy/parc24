d2a72e016bb260de62b4758260416a5c81f9c6a3
E-gy <balakhelisey@gmail.com>
2020-11-28 03:55:28 +0100

    tihs opts 🐛

--- src/main/tihs/opts.c
+++ src/main/tihs/opts.c
@@ -16,7 +16,7 @@ TihsOptsParseResult tihsopts_parse(argsarr args){
 	for(; *args && (strpref("-", *args) || strpref("+", *args)); args++){
 		if(streq("-c", *args)){
 			if(!*++args) return Error_T(tihsopts_parse_result, {"-c requires an argument"});
-			opts.commandstr = *args;
+			opts.commandstr = *args++;
 			break;
 		} else if(streq("-s", *args)) readfromstdin = true;
 		//TODO more short options
