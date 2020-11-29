41e3ceab5982a2205fecb2cde6fa6abbba519599
E-gy <balakhelisey@gmail.com>
2020-11-29 22:35:01 +0100

    Context include parser

--- src/api/parc24/context.h
+++ src/api/parc24/context.h
@@ -5,10 +5,12 @@
 #include "var_store.h"
 #include "io.h"
 #include <cppo/types.h>
+#include <calp/parser.h>
 
 struct parcontext {
 	/** @ref */ VarStore vars;
 	struct exe_opts exeopts;
 	ParC24IO io;
+	Parser parcer;
 };
 typedef struct parcontext* ParContext;
