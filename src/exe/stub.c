86798dda8190f62fc756b391f80e2701f97353d7
E-gy <balakhelisey@gmail.com>
2020-11-29 05:52:26 +0100

    Exe Stub does the things now

--- src/exe/stub.c
+++ src/exe/stub.c
@@ -3,6 +3,9 @@
 
 #include <parc24/io.h>
 #include <tihs/opts.h>
+#include <parc24/pars.h>
+#include <tihs/exe.h>
+#include <util/bufferio.h>
 
 int main(int argc, argsarr args){
 	const ParC24IO io = parc24io_fromstd();
@@ -12,4 +15,36 @@ int main(int argc, argsarr args){
 		return 1;
 	});
 	struct tihsopts opts = optpars.r.ok;
+	const Parser parcer = parcer_defolt_new();
+	if(!parcer){
+		io.log(LL_CRITICAL, "Failed to initalize parser. Report logs to CALP, thx.");
+		return 1;
+	}
+	if(opts.commandstr || opts.commandfile){
+		string_mut str = opts.commandstr;
+		if(!(str = opts.commandstr)){
+			BufferResult buff = buffer_from_file(opts.commandfile);
+			IfError_T(buff, err, {
+				io.log(LL_ERROR, "Failed to read file - %s", err.s);
+				return 1;
+			});
+			str = buffer_destr(buff.r.ok);
+		}
+		TihsExeResult exer = tihs_exestr(str, parcer, &opts, io);
+		IfError_T(exer, err, {
+			io.log(LL_ERROR, "Execution error - %s", err.s);
+			return 1;
+		});
+		return exer.r.ok;
+	} else while(true){
+		int lec = 0;
+		IfElse_T(io.readline(), line, {
+			if(!line) return lec;
+			TihsExeResult exer = tihs_exestr(line, parcer, &opts, io);
+			IfError_T(exer, err, { io.log(LL_ERROR, "Execution error - %s", err.s); });
+			IfOk_T(exer, ec, { lec = ec; });
+		}, err, {
+			io.log(LL_ERROR, "Failed to read from standard input - %s", err.s);
+		});
+	}
 }
