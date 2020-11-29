ec15d85884772579d2342f0b506a25c9690c74ec
E-gy <balakhelisey@gmail.com>
2020-11-29 22:09:03 +0100

    calp exe linux run-s impl

--- src/main/cppo/exe.c
+++ src/main/cppo/exe.c
@@ -70,6 +70,11 @@ ExeWaitResult exe_waitretcode(ChildProcessInfo proc){
 #include <sys/wait.h>
 #include <stdio.h>
 
+#include <util/argsarr_mut.h>
+#include <grammar/quotexpando.h>
+#include <calp/lexers.h>
+#include <util/buffer.h>
+
 struct childprocinf {
 	pid_t pid;
 };
@@ -104,6 +109,23 @@ ExeRunResult exe_runa(argsarr args, struct exe_opts opts){
 		if(waitpid(cpid, &cstatus, 0) < 0) return Error_T(exerun_result, {"wait failed"});
 	} else procinf->pid = cpid;
 	return Ok_T(exerun_result, procinf);
+}
+
+ExeRunResult exe_runs(string_mut cmd, struct exe_opts opts){
+	ArgsArr_Mut args = argsarrmut_new(16);
+	if(!args) return Error_T(exerun_result, {"Args new failed"});
+	for(string s = cmd; *s;){
+		LexerResult next = lexer_spacebegone(s, capture_word);
+		if(!IsOk_T(next)){
+			argsarrmut_destroy(args);
+			return Error_T(exerun_result, {"Args split failed"});
+		}
+		if(!IsOk(argsarrmut_append(args, buffer_destr(buffer_new_from(next.r.ok.start, next.r.ok.end-next.r.ok.start))))) return Error_T(exerun_result, {"Args split (append) failed"});
+		s = next.r.ok.next;
+	}
+	ExeRunResult rr = exe_runa(args, opts);
+	argsarrmut_destroy(args);
+	return rr;
 }
 
 ExeWaitResult exe_waitretcode(ChildProcessInfo proc){
