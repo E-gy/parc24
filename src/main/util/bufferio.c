4ad8f9cbb01995ee0c71576fb2b3427c2c854ad6
E-gy <balakhelisey@gmail.com>
2020-11-29 23:03:47 +0100

    buff io from fd

--- src/main/util/bufferio.c
+++ src/main/util/bufferio.c
@@ -2,10 +2,8 @@
 
 #include <stdio.h>
 
-BufferResult buffer_from_file(string file){
-	if(!file) return Error_T(buffer_from_result, {"No file specified"});
-	FILE* f = fopen(file, "r");
-	if(!f) return Error_T(buffer_from_result, {"Failed to read specified file"});
+BufferResult buffer_from_f(FILE* f){
+	if(!f) return Error_T(buffer_from_result, {"no file specified"});
 	Buffer b = buffer_new(4096);
 	if(!b){
 		fclose(f);
@@ -19,4 +17,17 @@ BufferResult buffer_from_file(string file){
 	}
 	fclose(f);
 	return Ok_T(buffer_from_result, b);
+}
+
+BufferResult buffer_from_file(string file){
+	if(!file) return Error_T(buffer_from_result, {"No file specified"});
+	FILE* f = fopen(file, "r");
+	if(!f) return Error_T(buffer_from_result, {"Failed to read specified file"});
+	return buffer_from_f(f);
+}
+
+BufferResult buffer_from_fd(fd_t fd){
+	FILE* f = fdopen(fd, "r");
+	if(!f) return Error_T(buffer_from_result, {"Failed to read from fd"});
+	return buffer_from_f(f);
 }
--- src/main/util/bufferio.h
+++ src/main/util/bufferio.h
@@ -6,3 +6,9 @@ Result_T(buffer_from_result, Buffer, string_v);
 #define BufferResult struct buffer_from_result
 
 BufferResult buffer_from_file(string file);
+
+/**
+ * @param file @consumes
+ * @return BufferResult 
+ */
+BufferResult buffer_from_fd(fd_t file);
