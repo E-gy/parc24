655cdf65185530cfc01d34ba36a482e785058de6
E-gy <balakhelisey@gmail.com>
2020-11-29 03:52:40 +0100

    ParCer exposed

--- src/api/parc24/pars.h
+++ src/api/parc24/pars.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <calp/grammar.h>
+#include <calp/ast.h>
 #include <calp/parser.h>
 
 Parser parcer_defolt_new(void);
 
-GroupId parcer_defolt_entrgrp(void);
+Result_T(parce_result, AST, string_v);
+#define ParceResult struct parce_result
+
+ParceResult parcer_parse(Parser parcer, string input);
