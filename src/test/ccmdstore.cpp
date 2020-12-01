f01f473c2229322d69e6c5e63fd55fe2121e9656
E-gy <balakhelisey@gmail.com>
2020-12-01 22:22:06 +0100

    there can not be not enough tests

--- src/test/ccmdstore.cpp
+++ src/test/ccmdstore.cpp
@@ -13,24 +13,27 @@ static int dummyf4(int argc, argsarr args){ return 0; }
 }
 
 SCENARIO("ccmds store is operational", "[ccmds store][parc24]"){
+	string cmd1 = GENERATE("aaa", "c", "zzzzz", "echo");
+	string cmd2 = GENERATE("bb", "jj", "k", "run");
+	string cmd3 = GENERATE("bbcc", "yyy", "kj", "goodbye");
 	GIVEN("an empty store"){
 		CCMDStore store = ccmdstore_new();
 		REQUIRE(!!store);
-		REQUIRE(!ccmdstore_get(store, "echo"));
-		REQUIRE(!ccmdstore_get(store, "run"));
-		REQUIRE(!ccmdstore_get(store, "goodbye"));
+		REQUIRE(!ccmdstore_get(store, cmd1));
+		REQUIRE(!ccmdstore_get(store, cmd2));
+		REQUIRE(!ccmdstore_get(store, cmd3));
 		THEN("operations are operational"){
-			REQUIRE(IsOk(ccmdstore_set(store, "echo", dummyf1)));
-			REQUIRE(IsOk(ccmdstore_set(store, "run", dummyf2)));
-			REQUIRE(IsOk(ccmdstore_set(store, "goodbye", dummyf3)));
-			REQUIRE(ccmdstore_get(store, "echo") == dummyf1);
-			REQUIRE(ccmdstore_get(store, "run") == dummyf2);
-			REQUIRE(ccmdstore_get(store, "goodbye") == dummyf3);
+			REQUIRE(IsOk(ccmdstore_set(store, cmd1, dummyf1)));
+			REQUIRE(IsOk(ccmdstore_set(store, cmd2, dummyf2)));
+			REQUIRE(IsOk(ccmdstore_set(store, cmd3, dummyf3)));
+			REQUIRE(ccmdstore_get(store, cmd1) == dummyf1);
+			REQUIRE(ccmdstore_get(store, cmd2) == dummyf2);
+			REQUIRE(ccmdstore_get(store, cmd3) == dummyf3);
 			AND_THEN("and replaceable"){
-				REQUIRE(IsOk(ccmdstore_set(store, "echo", dummyf4)));
+				REQUIRE(IsOk(ccmdstore_set(store, cmd1, dummyf4)));
 				REQUIRE(ccmdstore_get(store, "echo") == dummyf4);
-				REQUIRE(ccmdstore_get(store, "run") == dummyf2);
-				REQUIRE(ccmdstore_get(store, "goodbye") == dummyf3);
+				REQUIRE(ccmdstore_get(store, cmd2) == dummyf2);
+				REQUIRE(ccmdstore_get(store, cmd3) == dummyf3);
 			}
 		}
 		ccmdstore_destroy(store);
