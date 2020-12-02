5a898f4694421bb172f27cf13f5824ddfff7cccd
E-gy <balakhelisey@gmail.com>
2020-12-03 00:13:02 +0100

    uuuu wiiiiii

--- src/main/grammar/aliaser.c
+++ src/main/grammar/aliaser.c
@@ -10,23 +10,13 @@ static string_mut whitespaceskip(string_mut s){
 	return s;
 }
 
-static string_mut whitespacetrim(string_mut str){
-	string_mut s = whitespaceskip(str);
-	if(!*s) return s;
-	string_mut e = s;
-	for(; *e; e++);
-	for(e--; isspace(*e); e--);
-	e[1] = '\0';
-	return s;
-}
-
 RealiasResult realias(string args[], AliasStore s){
 	if(!args || !s) return Error_T(realias_result, {"invalid input"});
 	if(!args[0] || !(isalnum(args[0][0]) || args[0][0] == '-' || args[0][0] == '_')) return Ok_T(realias_result, null);
 	string_mut repl = aliastore_get(s, args[0]);
 	if(!repl) return Ok_T(realias_result, null);
 	const bool recurs = *repl && isspace(repl[strlen(repl)-1]);
-	ArgsArr_Mut aargs = exe_args_split(whitespacetrim(repl));
+	ArgsArr_Mut aargs = exe_args_split(whitespaceskip(repl));
 	if(!aargs) return Error_T(realias_result, {"failed to reconstruct arguments"});
 	if(recurs){
 		const RealiasResult a2 = realias(args+1, s);
