e2699cebfb6f73c8eb0f019b0c94e61d506a30cd
E-gy <balakhelisey@gmail.com>
2020-12-02 18:22:25 +0100

    capture & cleanup macros

--- src/main/util/null.h
+++ src/main/util/null.h
@@ -3,3 +3,5 @@
 #define null NULL
 
 #define new(_type, _var) _type _var = malloc(sizeof(*_var)); if(!_var) return null;
+#define captclean(v, cleanclo) __extension__({ __typeof__ (v) _v = v; cleanclo _v; })
+#define retclean(ret, cleanclo) return captclean(ret, cleanclo)
