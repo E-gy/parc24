311fc9a74e98e1e68b2ef77e966c9e7246777e8c
E-gy <balakhelisey@gmail.com>
2020-12-02 23:20:10 +0100

    aliaser

---
+++ src/main/grammar/aliaser.c
@@ -0,0 +1,27 @@
+#include "aliaser.h"
+
+#include <util/null.h>
+#include <util/string.h>
+#include <cppo/util.h>
+#include <ctype.h>
+
+RealiasResult realias(argsarr args, AliasStore s){
+	if(!args || !s) return Error_T(realias_result, {"invalid input"});
+	if(!args[0] || !(isalnum(args[0][0]) || args[0][0] == '-' || args[0][0] == '_')) return Ok_T(realias_result, null);
+	string_mut repl = aliastore_get(s, args[0]);
+	if(!repl) return Ok_T(realias_result, null);
+	const bool recurs = *repl && iswspace(repl[strlen(repl)-1]);
+	ArgsArr_Mut aargs = exe_args_split(repl);
+	if(!aargs) return Error_T(realias_result, {"failed to reconstruct arguments"});
+	if(recurs){
+		const RealiasResult a2 = realias(args+1, s);
+		if(!IsOk_T(a2)) retclean(a2, {argsarrmut_destroy(aargs);});
+		const ArgsArr_Mut aargs2 = a2.r.ok;
+		if(aargs2){
+			for(size_t i = 0; i < aargs2->size; i++) if(!IsOk(argsarrmut_append(aargs, aargs->args[i]))) retclean(Error_T(realias_result, {"failed to construct recurse"}), {argsarrmut_destroy(aargs2);argsarrmut_destroy(aargs);}); else aargs->args[i] = null;
+			argsarrmut_destroy(aargs2);
+		}
+	}
+	return Ok_T(realias_result, aargs);
+}
+
---
+++ src/main/grammar/aliaser.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <parc24/aliastore.h>
+#include <util/argsarr_mut.h>
+
+Result_T(realias_result, /** @nullable */ ArgsArr_Mut, string_v);
+#define RealiasResult struct realias_result
+
+/**
+ * @param args @ref
+ * @param aliases @ref
+ * @return @produces aliased args (`null` if no aliasing is necessary)
+ */
+RealiasResult realias(argsarr args, AliasStore aliases);
