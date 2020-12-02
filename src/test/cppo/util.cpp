b78893c46ccd04d97280f463060c3284259db5b3
E-gy <balakhelisey@gmail.com>
2020-12-02 21:21:07 +0100

    args join test os agnostic

--- src/test/cppo/util.cpp
+++ src/test/cppo/util.cpp
@@ -2,80 +2,79 @@
 using Catch::Matchers::Equals;
 
 extern "C" {
-#include <cppo.h>
+#include <cppo/util.h>
 #include <util/null.h>
 }
 
 SCENARIO("join arguments", "[args][util][cppo]"){
-	WHEN("given nothing"){
-		string_mut cmd = exe_args_join(null);
+	GIVEN("nothing"){
+		string_mut cmd = exe_args_join(null, false);
 		THEN("error with nothing"){
 			REQUIRE(cmd == (char*)null);
 		}
 		free(cmd);
 	}
-	WHEN("given empty"){
+	GIVEN("empty"){
 		string args[] = {null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("result is empty"){
 			REQUIRE_THAT(cmd, Equals(""));
 		}
 		free(cmd);
 	}
-	WHEN("given 1 arg"){
+	GIVEN("1 arg"){
 		string args[] = {"hello", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("result that arg"){
 			REQUIRE_THAT(cmd, Equals("hello"));
 		}
 		free(cmd);
 	}
-	WHEN("given 2 args"){
+	GIVEN("2 args"){
 		string args[] = {"hello", "world", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("they are space joined"){
 			REQUIRE_THAT(cmd, Equals("hello world"));
 		}
 		free(cmd);
 	}
-	WHEN("argument contains space"){
+	GIVEN("argument containing space"){
 		string args[] = {"hello world", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("it is quoted"){
 			REQUIRE_THAT(cmd, Equals("'hello world'"));
 		}
 		free(cmd);
 	}
-	WHEN("argument contains double quote"){
+	GIVEN("argument containing double quote"){
 		string args[] = {"hello\"world", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("it is quoted"){
 			REQUIRE_THAT(cmd, Equals("'hello\"world'"));
 		}
 		free(cmd);
 	}
-	WHEN("argument contains single quote"){
+	GIVEN("argument containing single quote"){
 		string args[] = {"hello'world", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("it is quoted and escaped"){
-			#ifdef _WIN32
-			REQUIRE_THAT(cmd, Equals("'hello''world'"));
-			#else
+			// #ifdef _WIN32
+			// REQUIRE_THAT(cmd, Equals("'hello''world'"));
+			// #endif
 			REQUIRE_THAT(cmd, Equals("'hello'\\''world'"));
-			#endif
 		}
 		free(cmd);
 	}
-	WHEN("multiple weird arguments"){
+	GIVEN("multiple weird arguments"){
 		string args[] = {"hello", "th'e're", "\"general\" Kenobi", "!", null};
-		string_mut cmd = exe_args_join(args);
+		string_mut cmd = exe_args_join(args, false);
 		THEN("everything is ookay"){
 			REQUIRE_THAT(cmd, Equals(
-			#ifdef _WIN32
-			"hello 'th''e''re' '\"general\" Kenobi' !"
-			#else
+			// #ifdef _WIN32
+			// "hello 'th''e''re' '\"general\" Kenobi' !"
+			// #else
 			"hello 'th'\\''e'\\''re' '\"general\" Kenobi' !"
-			#endif
+			// #endif
 			));
 		}
 		free(cmd);
