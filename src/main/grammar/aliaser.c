ea334c0631ffa4f6dee59de240673f2269a06ccf
E-gy <balakhelisey@gmail.com>
2020-12-02 23:46:38 +0100

    aliaser trim aliased values

--- src/main/grammar/aliaser.c
+++ src/main/grammar/aliaser.c
@@ -5,13 +5,28 @@
 #include <cppo/util.h>
 #include <ctype.h>
 
+static string_mut whitespaceskip(string_mut s){
+	for(; *s && isspace(*s); s++);
+	return s;
+}
+
+static string_mut whitespacetrim(string_mut str){
+	string_mut s = whitespaceskip(str);
+	if(!*s) return s;
+	string_mut e = s;
+	for(; *e; e++);
+	for(e--; isspace(*e); e--);
+	e[1] = '\0';
+	return s;
+}
+
 RealiasResult realias(string args[], AliasStore s){
 	if(!args || !s) return Error_T(realias_result, {"invalid input"});
 	if(!args[0] || !(isalnum(args[0][0]) || args[0][0] == '-' || args[0][0] == '_')) return Ok_T(realias_result, null);
 	string_mut repl = aliastore_get(s, args[0]);
 	if(!repl) return Ok_T(realias_result, null);
 	const bool recurs = *repl && iswspace(repl[strlen(repl)-1]);
-	ArgsArr_Mut aargs = exe_args_split(repl);
+	ArgsArr_Mut aargs = exe_args_split(whitespacetrim(repl));
 	if(!aargs) return Error_T(realias_result, {"failed to reconstruct arguments"});
 	if(recurs){
 		const RealiasResult a2 = realias(args+1, s);
