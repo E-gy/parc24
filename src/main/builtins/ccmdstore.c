7974dccf3402a509c0ef7f6da6a16c1174d98772
E-gy <balakhelisey@gmail.com>
2020-12-02 01:41:59 +0100

    code processed commands store use generic trie

--- src/main/builtins/ccmdstore.c
+++ src/main/builtins/ccmdstore.c
@@ -1,78 +1,34 @@
-#include <parc24/ccmdstore.h>
+// #include <parc24/ccmdstore.h>
+typedef void (*CCMD)(void);
+
+#define _TRIE_IMPL_T CCMD
+#include <util/trie.h>
+
+typedef Trie CCMDStore;
 
 #include <stdlib.h>
 #include <util/null.h>
 #include <util/string.h>
 
-//TODO we literally have a Trie used elsewhere ._.
-//we can certainly do better than a flippin linked list..? 
+static void ccmd_destroy(CCMD ccmd){}
 
-struct ccmdstoreel;
-typedef struct ccmdstoreel* CCMDSEl;
-struct ccmdstoreel {
-	string_mut cmd;
-	CCMD exe;
-	CCMDSEl next;
-};
-
-static CCMDSEl ccmdstoreel_new(string_mut cmd, CCMD exe){
-	if(!cmd || !exe) return null;
-	new(CCMDSEl, e);
-	*e = (struct ccmdstoreel){cmd, exe, null};
-	return e;
+static CCMD string_mut_clone(CCMD ccmd){
+	return ccmd;
 }
 
-struct ccmdstore {
-	CCMDSEl els;
-};
-
-CCMDStore ccmdstore_new(){
-	new(CCMDStore, s);
-	*s = (struct ccmdstore){null};
-	return s;
+CCMDStore ccmdstore_new(void){
+	return trie_new(ccmd_destroy);
 }
 
 void ccmdstore_destroy(CCMDStore s){
-	if(!s) return;
-	for(CCMDSEl e = s->els; e;){
-		CCMDSEl next = e->next;
-		free(e->cmd);
-		free(e);
-		e = next;
-	}
-	free(s);
+	return trie_destroy(s);
 }
 
 CCMD ccmdstore_get(CCMDStore s, string cmd){
-	if(!s || !cmd) return null;
-	for(CCMDSEl e = s->els; e; e = e->next){
-		int cmp = strcmp(e->cmd, cmd);
-		if(cmp == 0) return e->exe;
-		if(cmp > 0) return null;
-	}
-	return null;
+	return trie_get(s, cmd);
 }
 
-Result ccmdstore_set(CCMDStore s, string cmd, CCMD exe){
-	if(!s || !cmd || !exe) return Error;
-	string_mut cmdv = strdup(cmd);
-	if(!cmdv) return Error;
-	CCMDSEl* e = &s->els;
-	for(; *e && strcmp((*e)->cmd, cmd) < 0; e = &((*e)->next));
-	if(*e && streq((*e)->cmd, cmd)){
-		CCMDSEl el = *e;
-		free(el->cmd);
-		el->cmd = cmdv;
-		el->exe = exe;
-	} else {
-		CCMDSEl el = ccmdstoreel_new(cmdv, exe);
-		if(!el){
-			free(cmdv);
-			return Error;
-		}
-		el->next = *e;
-		*e = el;
-	}
-	return Ok;
+Result ccmdstore_set(CCMDStore store, string cmd, CCMD exe){
+	if(!exe) return Error;
+	return trie_add(store, cmd, exe);
 }
-
