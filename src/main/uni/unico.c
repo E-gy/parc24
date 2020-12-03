1c766b73a6ea6c846158cd7a89afde4405a593db
E-gy <balakhelisey@gmail.com>
2020-12-04 00:37:38 +0100

    ._.

--- src/main/uni/unico.c
+++ src/main/uni/unico.c
@@ -5,12 +5,14 @@
 TraverseASTResult parcontext_uniwait(TraverseASTResult r){
 	if(!IsOk_T(r)) return r;
 	switch(r.r.ok.type){
-		case TRAV_WAIT_CHILD:
+		case TRAV_WAIT_CHILD: {
 			ExeWaitResult cw = exe_waitretcode(r.r.ok.v.child);
 			return IsOk_T(cw) ? Ok_T(travast_result, {TRAV_COMPLETED, {.completed = cw.r.ok}}) : Error_T(travast_result, cw.r.error);
-		case TRAV_WAIT_THREAD:
+		}
+		case TRAV_WAIT_THREAD: {
 			ExeWaitResult cw = exethread_waitretcode(r.r.ok.v.thread);
 			return IsOk_T(cw) ? Ok_T(travast_result, {TRAV_COMPLETED, {.completed = cw.r.ok}}) : Error_T(travast_result, cw.r.error);
+		}
 		default: return r;
 	}
 }
