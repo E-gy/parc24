a99f244f03c2497204e0fd1f0f6abfc9219396d0
E-gy <balakhelisey@gmail.com>
2020-12-04 00:38:03 +0100

    aliaser aliasing stack to not realias

--- src/main/grammar/aliaser.c
+++ src/main/grammar/aliaser.c
@@ -10,16 +10,24 @@ static string_mut whitespaceskip(string_mut s){
 	return s;
 }
 
-RealiasResult realias(string args[], AliasStore s){
+typedef struct realisstek* RealiaStek;
+struct realisstek {
+	string av;
+	RealiaStek next;
+};
+
+static RealiasResult realias_(string args[], AliasStore s, RealiaStek stek){
 	if(!args || !s) return Error_T(realias_result, {"invalid input"});
 	if(!args[0] || !(isalnum(args[0][0]) || args[0][0] == '-' || args[0][0] == '_')) return Ok_T(realias_result, null);
 	string_mut repl = aliastore_get(s, args[0]);
 	if(!repl) return Ok_T(realias_result, null);
+	for(RealiaStek k = stek; k; k = k->next) if(streq(args[0], k->av)) return Ok_T(realias_result, null);;
 	const bool recurs = *repl && isspace(repl[strlen(repl)-1]);
 	ArgsArr_Mut aargs = exe_args_split(whitespaceskip(repl));
 	if(!aargs) return Error_T(realias_result, {"failed to reconstruct arguments"});
 	if(recurs){
-		const RealiasResult a2 = realias(args+1, s);
+		struct realisstek snext = {args[0], stek};
+		const RealiasResult a2 = realias_(args+1, s, &snext);
 		if(!IsOk_T(a2)) retclean(a2, {argsarrmut_destroy(aargs);});
 		const ArgsArr_Mut aargs2 = a2.r.ok;
 		if(aargs2){
@@ -33,3 +41,6 @@ RealiasResult realias(string args[], AliasStore s){
 	return Ok_T(realias_result, aargs);
 }
 
+RealiasResult realias(string args[], AliasStore s){
+	return realias_(args, s, null);
+}
