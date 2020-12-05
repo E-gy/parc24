2f2d9eda8c56bad7018fb439c7b083883419141f
E-gy <balakhelisey@gmail.com>
2020-12-05 17:55:20 +0100

    null terminated array length calculator util

--- src/main/util/null.h
+++ src/main/util/null.h
@@ -5,3 +5,5 @@
 #define new(_type, _var) _type _var = malloc(sizeof(*_var)); if(!_var) return null;
 #define captclean(v, cleanclo) __extension__({ __typeof__ (v) _v = v; cleanclo _v; })
 #define retclean(ret, cleanclo) return captclean(ret, cleanclo)
+
+#define ntarrlen(arr) __extension__({ size_t l = 0; for(__typeof__ (arr) _a = arr; _a && *_a; _a++) l++; l; })
