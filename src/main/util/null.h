4c9a7a77def0a86c468bd5d6f27189c74c1d6e76
E-gy <balakhelisey@gmail.com>
2020-12-05 18:50:02 +0100

    array length macro now with variable int types

--- src/main/util/null.h
+++ src/main/util/null.h
@@ -6,4 +6,5 @@
 #define captclean(v, cleanclo) __extension__({ __typeof__ (v) _v = v; cleanclo _v; })
 #define retclean(ret, cleanclo) return captclean(ret, cleanclo)
 
-#define ntarrlen(arr) __extension__({ size_t l = 0; for(__typeof__ (arr) _a = arr; _a && *_a; _a++) l++; l; })
+#define ntarrlen_t(arr, intt) __extension__({ intt l = 0; for(__typeof__ (arr) _a = arr; _a && *_a; _a++) l++; l; })
+#define ntarrlen(arr) ntarrlen_t(arr, size_t)
