2694ab39fe60075fd6b1df64d9e512d33c85c195
E-gy <balakhelisey@gmail.com>
2020-12-05 22:00:02 +0100

    string 2 int 🐛

--- src/main/util/str2i.c
+++ src/main/util/str2i.c
@@ -7,5 +7,5 @@ Str2IResult str2ir(string s, int r){
 	if(!s) return Error_T(str2i_result, null);
 	string_mut end = null;
 	long l = strtol(s, &end, r);
-	return end ? Ok_T(str2i_result, l) : Error_T(str2i_result, null);
+	return end && end != s ? Ok_T(str2i_result, l) : Error_T(str2i_result, null);
 }
