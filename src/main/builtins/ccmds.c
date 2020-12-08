3bb6e060c5276b503526804ee5d92efa91b43b43
E-gy <balakhelisey@gmail.com>
2020-12-08 21:33:23 +0100

    ccmds background info not necessary

--- src/main/builtins/ccmds.c
+++ src/main/builtins/ccmds.c
@@ -10,14 +10,14 @@
 
 struct pakkedi {
 	ArgsArr_Mut args;
-	struct exe_opts exeopts;
+	IOsStack ios;
 };
 typedef struct pakkedi* TSPA;
 
 static void pakked_destroy(TSPA a){
 	if(!a) return;
 	argsarrmut_destroy(a->args);
-	iosstack_destroy(a->exeopts.iostreams);
+	iosstack_destroy(a->ios);
 	free(a);
 }
 
@@ -25,17 +25,17 @@ static TSPA pakked_new(argsarr args, ParContext c){
 	if(!args || !c) return null;
 	new(TSPA, p);
 	*p = (struct pakkedi){0};
-	*p = (struct pakkedi){argsarrmut_from(cpt2ptr(args)), { iosstack_snapdup(c->exeopts.iostreams), c->exeopts.background }};
+	*p = (struct pakkedi){argsarrmut_from(cpt2ptr(args)), iosstack_snapdup(c->ios)};
 	if(!p->args) retclean(null, {pakked_destroy(p);});
 	return p;
 }
 
 static int cmd_echo_exe(TSPA a){
 	for(size_t i = 1; i < a->args->size; i++){
-		if(!IsOk(fddio_writestr(iosstack_raw_get(a->exeopts.iostreams, IOSTREAM_STD_OUT), a->args->args[i]))) retclean(1, { pakked_destroy(a); });
-		if(i < a->args->size-1) if(!IsOk(fddio_writestr(iosstack_raw_get(a->exeopts.iostreams, IOSTREAM_STD_OUT), " "))) retclean(1, { pakked_destroy(a); });
+		if(!IsOk(fddio_writestr(iosstack_raw_get(a->ios, IOSTREAM_STD_OUT), a->args->args[i]))) retclean(1, { pakked_destroy(a); });
+		if(i < a->args->size-1) if(!IsOk(fddio_writestr(iosstack_raw_get(a->ios, IOSTREAM_STD_OUT), " "))) retclean(1, { pakked_destroy(a); });
 	}
-	if(!IsOk(fddio_writestr(iosstack_raw_get(a->exeopts.iostreams, IOSTREAM_STD_OUT), "\n"))) retclean(1, { pakked_destroy(a); });
+	if(!IsOk(fddio_writestr(iosstack_raw_get(a->ios, IOSTREAM_STD_OUT), "\n"))) retclean(1, { pakked_destroy(a); });
 	retclean(0, { pakked_destroy(a); });
 }
 static threadfwrap_reti(cmd_echo_exe);
@@ -43,6 +43,6 @@ static threadfwrap_reti(cmd_echo_exe);
 TraverseASTResult cmd_echo(argsarr args, ParContext context){
 	TSPA a = pakked_new(args, context);
 	if(!a) return Error_T(travast_result, {"failed to pack up parallels args"});
-	ThreadResult tr = parallels_runf(cmd_echo_exe_wrap, a, context->exeopts.background);
+	ThreadResult tr = parallels_runf(cmd_echo_exe_wrap, a, context->exeback);
 	return IsOk_T(tr) ? Ok_T(travast_result, {TRAV_WAIT_THREAD, {.thread = tr.r.ok}}) : Error_T(travast_result, tr.r.error);
 }
