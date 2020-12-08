cec4f2d4d65e3de4c98e1a58420904095c964ebe
E-gy <balakhelisey@gmail.com>
2020-12-08 22:27:33 +0100

    tihs options tests converted to parcio

--- src/test/tihs/opts.cpp
+++ src/test/tihs/opts.cpp
@@ -4,11 +4,13 @@ using Catch::Matchers::Equals;
 extern "C" {
 #include <tihs/opts.h>
 #include <util/null.h>
+#include <parc24/ioslog.h>
 }
 
 SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
+	IOsStack ios = parcio_new_fromstd();
 	WHEN("nada args"){
-		TihsOptsParseResult res = tihsopts_parse_caste(null, parc24io_fromstd());
+		TihsOptsParseResult res = tihsopts_parse_caste(null, ios);
 		THEN("error"){
 			REQUIRE(!IsOk_T(res));
 		}
@@ -16,7 +18,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 	WHEN("no args"){
 		string args[] = {null};
 		THEN("parsing ok"){
-			IfElse_T(tihsopts_parse_caste(args, parc24io_fromstd()), opts, {
+			IfElse_T(tihsopts_parse_caste(args, ios), opts, {
 				AND_THEN("read from stdin"){
 					REQUIRE(!opts.commandstr);
 					REQUIRE(!opts.commandfile);
@@ -29,7 +31,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 	WHEN("single arg"){
 		string args[] = {"file.script", null};
 		THEN("parsing ok"){
-			IfElse_T(tihsopts_parse_caste(args, parc24io_fromstd()), opts, {
+			IfElse_T(tihsopts_parse_caste(args, ios), opts, {
 				AND_THEN("read from file and positional args start at the file"){
 					REQUIRE(!opts.commandstr);
 					REQUIRE_THAT(opts.commandfile, Equals(args[0]));
@@ -42,7 +44,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 	WHEN("explicit stdin"){
 		string args[] = {"-s", null};
 		THEN("parsing ok"){
-			IfElse_T(tihsopts_parse_caste(args, parc24io_fromstd()), opts, {
+			IfElse_T(tihsopts_parse_caste(args, ios), opts, {
 				AND_THEN("read from stdin"){
 					REQUIRE(!opts.commandstr);
 					REQUIRE(!opts.commandfile);
@@ -55,7 +57,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 	WHEN("read from string"){
 		WHEN("args end too early"){
 			string args[] = {"-c", null};
-			TihsOptsParseResult res = tihsopts_parse_caste(args, parc24io_fromstd());
+			TihsOptsParseResult res = tihsopts_parse_caste(args, ios);
 			THEN("error"){
 				REQUIRE(!IsOk_T(res));
 			}
@@ -63,7 +65,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 		WHEN("it's the last arg"){
 			string args[] = {"-c", "echo 'dub dub'", null};
 			THEN("parsing ok"){
-				IfElse_T(tihsopts_parse_caste(args, parc24io_fromstd()), opts, {
+				IfElse_T(tihsopts_parse_caste(args, ios), opts, {
 					AND_THEN("read from the string"){
 						REQUIRE_THAT(opts.commandstr, Equals(args[1]));
 						REQUIRE(!opts.commandfile);
@@ -76,7 +78,7 @@ SCENARIO("deciding where to read from", "[opts][parse][tihs]"){
 		WHEN("it isn't the last arg"){
 			string args[] = {"-c", "echo 'dub dub'", "-s", "-c", "ping pong", null};
 			THEN("parsing ok"){
-				IfElse_T(tihsopts_parse_caste(args, parc24io_fromstd()), opts, {
+				IfElse_T(tihsopts_parse_caste(args, ios), opts, {
 					AND_THEN("read from the string and everything after it is not parsed"){
 						REQUIRE_THAT(opts.commandstr, Equals(args[1]));
 						REQUIRE(!opts.commandfile);
