62c615aa27477383a342cb122e5ec9288952740b
E-gy <balakhelisey@gmail.com>
2020-12-12 00:09:12 +0100

    patterns impls for public use

---
+++ src/main/pat/pat.c
@@ -0,0 +1,34 @@
+#include "patpars.h"
+
+#include <util/null.h>
+#include <stdlib.h>
+
+PatternCompiler patcomp_new(void){
+	new(PatternCompiler, c);
+	*c = (struct pattern_compiler){patpars_new(), null /*TODO*/};
+	return c;
+}
+
+void patcomp_destroy(PatternCompiler pc){
+	if(!pc) return;
+	parser_destroy(pc->standard);
+	parser_destroy(pc->extended);
+	free(pc);
+}
+
+void pattern_destroy(Pattern p){
+	auto_destroy(p);
+}
+
+PatCompResult pattern_parse_ext(Parser parser, string str, bool doublestar){
+	return Error_T(patcomp_result, {"UNIMPLEMENTED"});
+}
+
+PatCompResult pattern_compile(PatternCompiler pc, string pat, struct parc_options opts){
+	if(!pc || !pat) return Error_T(patcomp_result, {"invalid arguments"});
+	return (opts.extglob ? pattern_parse_ext : pattern_parse)(opts.extglob ? pc->extended : pc->standard, pat, false /* y the excuse me fork did i implement it then? */);
+}
+
+bool pattern_test(Pattern p, string str){
+	return auto_test(p, str);
+}
