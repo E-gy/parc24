6895af0bc961b916df01a94b4ac00d16d57b7270
E-gy <balakhelisey@gmail.com>
2020-12-19 19:40:03 +0100

    stub exe messages & exit code fixes

--- src/exe/stub.c
+++ src/exe/stub.c
@@ -20,7 +20,7 @@ static int dothething(struct tihsopts opts, ParContext ctxt){
 			BufferResult buff = buffer_from_file(opts.commandfile);
 			IfError_T(buff, err, {
 				parciolog(ctxt->ios, LL_ERROR, "Failed to read file - %s", err.s);
-				return 1;
+				return 2;
 			});
 			str = buffer_destr(buff.r.ok);
 		}
@@ -28,7 +28,7 @@ static int dothething(struct tihsopts opts, ParContext ctxt){
 		if(str != opts.commandstr) free(str);
 		IfError_T(exer, err, {
 			parciolog(ctxt->ios, LL_ERROR, "Execution error - %s", err.s);
-			return 1;
+			return 2;
 		});
 		return exer.r.ok.code;
 	} else for(bool exit = false; !exit;){
@@ -56,7 +56,7 @@ static int dothething(struct tihsopts opts, ParContext ctxt){
 				}
 				buffer_destroy(li);
 			} else free(line);
-			IfError_T(exer, err, { parciolog(ctxt->ios, LL_ERROR, "Execution error - %s", err.err.s); });
+			IfError_T(exer, err, { parciolog(ctxt->ios, LL_ERROR, "Execution error - %s", err.err.s); ctxt->lastexit = 2; });
 			IfOk_T(exer, ec, { ctxt->lastexit = ec.code; exit = ec.exit; });
 		}, err, {
 			parciolog(ctxt->ios, LL_ERROR, "Failed to read from standard input - %s", err.s);
@@ -69,38 +69,38 @@ int main(ATTR_UNUSED int argc, argsarr args){
 	IOsStack ios = parcio_new_fromstd();
 	if(!ios){
 		fprintf(stderr, "[SUPERCRITICAL] Failed to create io stack\n");
-		return 1;
+		return 3;
 	}
 	TihsOptsParseResult optpars = tihsopts_parse_caste(args+1, ios);
 	IfError_T(optpars, err, {
 		parciolog(ios, LL_CRITICAL, "Failed to parse arguments - %s", err.s);
-		return 1;
+		return 3;
 	});
 	struct tihsopts opts = optpars.r.ok;
 	const Parser parcer = parcer_defolt_new();
 	if(!parcer){
-		parciolog(ios, LL_CRITICAL, "Failed to initalize parser. Report logs to CALP, thx.");
-		return 1;
+		parciolog(ios, LL_CRITICAL, "Failed to initalize parser.");
+		return 3;
 	}
 	WorkDirStack wd = wdstack_new();
 	if(!wd){
 		parciolog(ios, LL_CRITICAL, "Failed to create work dir stack");
-		return 1;
+		return 3;
 	}
 	VarStore vars = varstore_new();
 	if(!vars || !IsOk(varstore_add(vars, "IFS", " \t\n"))){
 		parciolog(ios, LL_CRITICAL, "Failed to initialize variables store");
-		return 1;
+		return 3;
 	}
 	FuncStore funcs = funcstore_new();
 	if(!funcs){
 		parciolog(ios, LL_CRITICAL, "Failed to create functions store");
-		return 1;
+		return 3;
 	}
 	CCMDStore ccmds = ccmdstore_new();
 	if(!ccmds){
 		parciolog(ios, LL_CRITICAL, "Failed to create ccmds store");
-		return 1;
+		return 3;
 	}
 	if(
 		!IsOk(ccmdstore_set(ccmds, "exit", cmd_exit)) ||
@@ -116,22 +116,22 @@ int main(ATTR_UNUSED int argc, argsarr args){
 		!IsOk(ccmdstore_set(ccmds, "shopt", cmd_shopt))
 	){
 		parciolog(ios, LL_CRITICAL, "Failed to register builtin ccmds");
-		return 1;
+		return 3;
 	}
 	AliasStore aliases = aliastore_new();
 	if(!ccmds){
 		parciolog(ios, LL_CRITICAL, "Failed to create aliases store");
-		return 1;
+		return 3;
 	}
 	PatternCompiler patco = patcomp_new();
 	if(!patco){
-		parciolog(ios, LL_CRITICAL, "Failed to create aliases store");
-		return 1;
+		parciolog(ios, LL_CRITICAL, "Failed to initialize patterns");
+		return 3;
 	}
 	Arithmetics arith = arith_new();
 	if(!arith){
-		parciolog(ios, LL_CRITICAL, "Failed to create arithmetics calculator");
-		return 1;
+		parciolog(ios, LL_CRITICAL, "Failed to initialize arithmetics");
+		return 3;
 	}
 	struct parcontext ctxt = {vars, vars, funcs, ccmds, aliases, ios, wd, patco, arith, args[0], opts.args, 0, false, &opts.parcopts, parcer};
 	int ec = dothething(opts, &ctxt);
