d9cf0482b0e92c1930a1561847aee7acd261344b
E-gy <balakhelisey@gmail.com>
2020-12-20 03:04:45 +0100

    cppo fd info query

---
+++ src/main/cppo/fdi.c
@@ -0,0 +1,35 @@
+#include "fdi.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#ifdef _WIN32
+
+static Result fdgetflags(ATTR_UNUSED fd_t fd, int* flags){
+	*flags = O_RDONLY&O_WRONLY;
+	return Ok; //FIXME eh:/
+}
+
+#else
+
+static Result fdgetflags(fd_t fd, int* flags){
+	return (*flags = fcntl(fd, F_GETFL)) < 0 ? Error : Ok;
+}
+
+#endif
+
+bool fdisreadable(fd_t fd){
+	int flags;
+	return IsOk(fdgetflags(fd,&flags)) && flags&O_RDONLY;
+}
+
+bool fdiswriteable(fd_t fd){
+	int flags;
+	return IsOk(fdgetflags(fd,&flags)) && flags&O_WRONLY;
+}
+
+bool fdisreadwriteable(fd_t fd){
+	int flags;
+	return IsOk(fdgetflags(fd,&flags)) && flags&O_RDONLY && flags&O_WRONLY;
+
+}
---
+++ src/main/cppo/fdi.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <ptypes.h>
+
+bool fdisreadable(fd_t fd);
+bool fdiswriteable(fd_t fd);
+bool fdisreadwriteable(fd_t fd);
