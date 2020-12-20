c013faf78d6c6d55adbe3114b462e1cb7296b4ca
E-gy <balakhelisey@gmail.com>
2020-12-20 06:14:30 +0100

    ccmd echo backslash processing

--- src/main/builtins/ccmds.c
+++ src/main/builtins/ccmds.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <cppo/parallels.h>
 #include <parc24/ioslog.h>
+#include <util/buffer.h>
 
 struct pakkedi {
 	ArgsArr_Mut args;
@@ -31,12 +32,57 @@ static TSPA pakked_new(argsarr args, ParContext c){
 	return p;
 }
 
+#define escnumbasedigi(digi, base) { \
+	char mrk = 0; \
+	size_t j; \
+	for(j = i+2; j < i+2+digi && j < buff->size; j++); \
+	if(j == i+2+digi){ \
+		mrk = buff->data[j]; \
+		buff->data[j] = '\0'; \
+	} \
+	string_mut ent = null; \
+	long hex = strtol(buff->data+i+2, &ent, base); \
+	if(mrk) buff->data[j] = mrk; \
+	if(!ent || ent == buff->data+i+2){ \
+		repld = 0; break; \
+	} \
+	repl[0] = hex; \
+	repld = ent-(buff->data+i); \
+	break; \
+}
+
 static int cmd_echo_exe(TSPA a){
 	bool omitnl = false;
-	ATTR_UNUSED bool bakslsh = a->opts.xpg_echo; //TODO baskslsh processing
+	ATTR_UNUSED bool bakslsh = a->opts.xpg_echo;
 	size_t i = 1;
 	for(; i < a->args->size; i++) if(streq("-n", a->args->args[i])) omitnl = true; else if(streq("-e", a->args->args[i])) bakslsh = true; else if(streq("-E", a->args->args[i])) bakslsh = false; else break;
-	for(; i < a->args->size; i++) parcioprintf(a->ios, LL_INFO, "%s%s", a->args->args[i], i < a->args->size-1 ? " " : "");
+	bool juststop = false;
+	for(; i < a->args->size && !juststop; i++) if(bakslsh){
+		Buffer buff = buffer_new_from(a->args->args[i], -1);
+		if(!buff) retclean(2, { pakked_destroy(a); });
+		for(size_t i = 0; i < buff->size && !juststop; i++) if(buff->data[i] == '\\'){
+			char repl[] = { 0, 0 };
+			size_t repld = 2;
+			switch(buff->data[i+1]){
+				case 'a': repl[0] = '\a'; break;
+				case 'b': repl[0] = '\b'; break;
+				case 'c': juststop = true; break;
+				case 'e': repl[0] = 27; break;
+				case 'f': repl[0] = '\f'; break;
+				case 'n': repl[0] = '\n'; break;
+				case 'r': repl[0] = '\r'; break;
+				case 't': repl[0] = '\t'; break;
+				case 'v': repl[0] = '\v'; break;
+				case '\\': repl[0] = '\\'; break;
+				case 'x': escnumbasedigi(2, 16);
+				case '0': escnumbasedigi(3, 8);
+				default: repld = 0;
+			}
+			if(repld > 0) buffer_splice_str(buff, i, i+repld, repl);
+		}
+		parcioprintf(a->ios, LL_INFO, "%s%s", buff->data, i < a->args->size-1 ? " " : "");
+		buffer_destroy(buff);
+	} else parcioprintf(a->ios, LL_INFO, "%s%s", a->args->args[i], i < a->args->size-1 ? " " : "");
 	if(!omitnl) parcioprintf(a->ios, LL_INFO, "\n");
 	retclean(0, { pakked_destroy(a); });
 }
