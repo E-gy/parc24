53ab5facf6397bff913a3de5fff1a5936c7d4a2b
E-gy <balakhelisey@gmail.com>
2020-12-20 08:51:10 +0100

    ctrl ccmds args handlings

--- src/main/builtins/ctrl.c
+++ src/main/builtins/ctrl.c
@@ -5,21 +5,51 @@
 #include <util/str2i.h>
 #include <util/argsarr_mut.h>
 #include <stdlib.h>
+#include <parc24/ioslog.h>
 
 TraverseASTResult cmd_exit(argsarr args, ATTR_UNUSED ParContext context){
 	int code = context->lastexit;
-	if(args[1]) IfOk_T(str2i(args[1]), c, { code = c; });
+	if(args[1]){
+		Str2IResult ps = str2i(args[1]);
+		if(!IsOk_T(ps)){
+			parciolog(context->ios, LL_ERROR, "%s: numeric argument required", args[0]);
+			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 2}});
+		}
+		code = ps.r.ok;
+	}
 	return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = code}});
 }
 
 TraverseASTResult cmd_break(argsarr args, ATTR_UNUSED ParContext context){
 	int lvl = 1;
-	if(args[1]) IfOk_T(str2i(args[1]), l, { if(l > 0) lvl = l; });
+	if(args[1]){
+		Str2IResult ps = str2i(args[1]);
+		if(!IsOk_T(ps)){
+			parciolog(context->ios, LL_ERROR, "%s: numeric argument required", args[0]);
+			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+		}
+		lvl = ps.r.ok;
+		if(lvl < 0){
+			parciolog(context->ios, LL_ERROR, "%s: positive argument required", args[0]);
+			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 1}});
+		}
+	}
 	return Ok_T(travast_result, {TRAV_SHRTCT_BREAK, {.shortcut_depth = lvl}});
 }
 
 TraverseASTResult cmd_continue(argsarr args, ATTR_UNUSED ParContext context){
 	int lvl = 1;
-	if(args[1]) IfOk_T(str2i(args[1]), l, { if(l > 0) lvl = l; });
+	if(args[1]){
+		Str2IResult ps = str2i(args[1]);
+		if(!IsOk_T(ps)){
+			parciolog(context->ios, LL_ERROR, "%s: numeric argument required", args[0]);
+			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+		}
+		lvl = ps.r.ok;
+		if(lvl < 0){
+			parciolog(context->ios, LL_ERROR, "%s: positive argument required", args[0]);
+			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 1}});
+		}
+	}
 	return Ok_T(travast_result, {TRAV_SHRTCT_CONTINUE, {.shortcut_depth = lvl}});
 }
