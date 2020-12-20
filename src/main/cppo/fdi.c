bec83e1abfab9c41845d72985b129e05a4353239
E-gy <balakhelisey@gmail.com>
2020-12-20 03:25:24 +0100

    uuf

--- src/main/cppo/fdi.c
+++ src/main/cppo/fdi.c
@@ -20,16 +20,15 @@ static Result fdgetflags(fd_t fd, int* flags){
 
 bool fdisreadable(fd_t fd){
 	int flags;
-	return IsOk(fdgetflags(fd,&flags)) && flags&O_RDONLY;
+	return IsOk(fdgetflags(fd,&flags)) && (flags&O_RDONLY) == O_RDONLY;
 }
 
 bool fdiswriteable(fd_t fd){
 	int flags;
-	return IsOk(fdgetflags(fd,&flags)) && flags&O_WRONLY;
+	return IsOk(fdgetflags(fd,&flags)) && (flags&O_WRONLY) == O_WRONLY;
 }
 
 bool fdisreadwriteable(fd_t fd){
 	int flags;
-	return IsOk(fdgetflags(fd,&flags)) && flags&O_RDONLY && flags&O_WRONLY;
-
+	return IsOk(fdgetflags(fd,&flags)) && (flags&O_RDWR) == O_RDWR;
 }
