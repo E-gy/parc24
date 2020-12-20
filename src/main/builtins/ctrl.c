4cb1f219a5fc1b8a435588a08b26db61f2a04996
E-gy <balakhelisey@gmail.com>
2020-12-20 10:05:34 +0100

    ctrl ccmds very wrong arguments baktrak hijak

--- src/main/builtins/ctrl.c
+++ src/main/builtins/ctrl.c
@@ -23,19 +23,19 @@ TraverseASTResult cmd_exit(argsarr args, ParContext context){
 TraverseASTResult cmd_break(argsarr args, ParContext context){
 	if(context->lupdepth == 0){
 		parciolog(context->ios, LL_ERROR, "%s: makes no sense outside looops", args[0]);
-		return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+		return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 128}});
 	}
 	int lvl = 1;
 	if(args[1]){
 		Str2IResult ps = str2i(args[1]);
 		if(!IsOk_T(ps)){
 			parciolog(context->ios, LL_ERROR, "%s: numeric argument required", args[0]);
-			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+			return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 128}});
 		}
 		lvl = ps.r.ok;
 		if(lvl < 0){
 			parciolog(context->ios, LL_ERROR, "%s: positive argument required", args[0]);
-			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 1}});
+			return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 1}});
 		}
 	}
 	return Ok_T(travast_result, {TRAV_SHRTCT_BREAK, {.shortcut_depth = lvl > context->lupdepth ? context->lupdepth : lvl}});
@@ -44,19 +44,19 @@ TraverseASTResult cmd_break(argsarr args, ParContext context){
 TraverseASTResult cmd_continue(argsarr args, ParContext context){
 	if(context->lupdepth == 0){
 		parciolog(context->ios, LL_ERROR, "%s: makes no sense outside looops", args[0]);
-		return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+		return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 128}});
 	}
 	int lvl = 1;
 	if(args[1]){
 		Str2IResult ps = str2i(args[1]);
 		if(!IsOk_T(ps)){
 			parciolog(context->ios, LL_ERROR, "%s: numeric argument required", args[0]);
-			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 128}});
+			return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 128}});
 		}
 		lvl = ps.r.ok;
 		if(lvl < 0){
 			parciolog(context->ios, LL_ERROR, "%s: positive argument required", args[0]);
-			return Ok_T(travast_result, {TRAV_COMPLETED, {.completed = 1}});
+			return Ok_T(travast_result, {TRAV_SHRTCT_EXIT, {.completed = 1}});
 		}
 	}
 	return Ok_T(travast_result, {TRAV_SHRTCT_CONTINUE, {.shortcut_depth = lvl > context->lupdepth ? context->lupdepth : lvl}});
